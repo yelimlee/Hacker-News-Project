31242311de307f833b48365ed997672259c4e39a
yelimlee <limroman@naver.com>
2021-07-14 22:36:34 +0900

    axios

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-      ask
+      <div v-for="item in ask" v-bind:key="item">{{ item.title }}</div>
   </div>
 </template>
 
 <script>
+import {fetchAskList} from '../api/index.js'
 export default {
-
+  data(){
+    return{
+      ask:[]
+    }
+  },
+  created(){
+    fetchAskList()
+    .then(response => this.ask=response.data)
+    .catch(error=> console.log(error))
+  }
 }
 </script>
 
